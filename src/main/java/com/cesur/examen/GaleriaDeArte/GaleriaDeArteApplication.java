e1454e41be31fe6c070afda305e14cd4e10a965c
ProfeFS <profefs@outlook.com>
2023-11-08 09:59:59 +0100

    Creado proyecto Galería de arte

---
+++ src/main/java/com/cesur/examen/GaleriaDeArte/GaleriaDeArteApplication.java
@@ -0,0 +1,13 @@
+package com.cesur.examen.GaleriaDeArte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaleriaDeArteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaleriaDeArteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesur/examen/GaleriaDeArte/GaleriaDeArteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesur.examen.GaleriaDeArte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaleriaDeArteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
