c9eba305d1f5a11e01e2697f687a9998911a7f63
ProfeFS <profefs@outlook.com>
2023-11-15 11:21:51 +0100

    Agregada anotación repository

--- src/main/java/com/cesur/examen/GaleriaDeArte/persistence/repositories/ArtistRepository.java
+++ src/main/java/com/cesur/examen/GaleriaDeArte/persistence/repositories/ArtistRepository.java
@@ -1,8 +1,11 @@
 package com.cesur.examen.GaleriaDeArte.persistence.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.cesur.examen.GaleriaDeArte.persistence.entities.ArtistEntity;
 
+@Repository
 public interface ArtistRepository extends JpaRepository<ArtistEntity, Integer>{
 
 }
--- src/main/java/com/cesur/examen/GaleriaDeArte/persistence/repositories/ArtworkRepository.java
+++ src/main/java/com/cesur/examen/GaleriaDeArte/persistence/repositories/ArtworkRepository.java
@@ -1,9 +1,11 @@
 package com.cesur.examen.GaleriaDeArte.persistence.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cesur.examen.GaleriaDeArte.persistence.entities.ArtworkEntity;
 
+@Repository
 public interface ArtworkRepository extends JpaRepository<ArtworkEntity, Integer>{
 
 }
